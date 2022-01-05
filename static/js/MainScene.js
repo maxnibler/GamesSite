de9caf7ff160d026ae0e87e5e91db48d0ef5ac74
maxnibler <max.nibler@gmail.com>
2022-01-04 16:17:37 -0800

    reformatted and added scenes

---
+++ static/js/MainScene.js
@@ -0,0 +1,59 @@
+export default class MainScene extends Phaser.Scene {
+    constructor () {
+        super('Main');
+    }
+
+    preload () {
+        this.load.image('tiles', 'assets/tiles/drawtiles1.png');
+        this.load.tilemapTiledJSON('map', 'assets/tilemaps/dungeon.json');
+        this.load.image('sidemenu bkgd', 'assets/UI/sidemenu background.png');
+        this.load.image('gold-icon', 'assets/UI/gold coin.jpg');
+    }
+
+    create () {
+        this.scene.run('game-ui');
+        //Create tilemap
+        this.map = this.make.tilemap({ key: 'map' });
+        //Add tileset to this.map
+        const tileset = this.map.addTilesetImage('Cave', 'tiles');
+        //Load the layer from tileset
+        const layer1 = this.map.createLayer('Floor1', tileset, 0, 0);
+
+        this.currentTile = this.map.getTileAt(1,1);
+    
+        this.marker = this.add.graphics();
+        this.marker.lineStyle(2, 0x000000, 1);
+        this.marker.strokeRect(0, 0, this.map.tileWidth, this.map.tileHeight);
+
+        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    
+        const cursors = this.input.keyboard.createCursorKeys();
+        const controlConfig = {
+            camera: this.cameras.main,
+            left: cursors.left,
+            right: cursors.right,
+            up: cursors.up,
+            down: cursors.down,
+            speed: 0.5,
+        };
+        
+        this.controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
+    }
+
+    update (tile, delta) {
+        this.controls.update(delta);
+    
+        const worldPoint = this.input.activePointer.positionToCamera(this.cameras.main);
+
+        const pointerTileX = this.map.worldToTileX(worldPoint.x);
+        const pointerTileY = this.map.worldToTileY(worldPoint.y);
+    
+        this.marker.x = this.map.tileToWorldX(pointerTileX);
+        this.marker.y = this.map.tileToWorldY(pointerTileY);
+
+        if (this.input.manager.activePointer.isDown) {
+            this.map.putTileAt(this.currentTile, pointerTileX, pointerTileY);
+        }
+        
+    }
+}
--- static/js/dungeonmaster.js
+++ static/js/dungeonmaster.js
@@ -1,77 +1,19 @@
-var config = {
+import MainScene from "./MainScene.js";
+import GameUI from "./gameUI.js";
+
+const config = {
     type: Phaser.AUTO,
     width: 900,
     height: 600,
     parent: "gameDiv",
     backgroundColor: '#efefef',
-    scene: {
-        preload: preload,
-        create: create,
-        update: update,
-    }
-};
-
-var game = new Phaser.Game(config);
-
-function preload () {
-    this.load.image('tiles', 'assets/tiles/drawtiles1.png');
-    this.load.tilemapTiledJSON('map', 'assets/tilemaps/dungeon.json');
-    this.load.image('sidemenu bkgd', 'assets/UI/sidemenu background.png');
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+    },
+    scene: [MainScene, GameUI]
 }
 
-var map;
-var tileset;
-var layer1;
-var currentTile;
-var marker;
-var controls;
-var menuContainer;
-
-function create () {
-    //Create tilemap
-    map = this.make.tilemap({ key: 'map' });
-    //Add tileset to map
-    tileset = map.addTilesetImage('Cave', 'tiles');
-    //Create Blank Layer
-    layer1 = map.createLayer('Floor1', tileset, 0, 0);
-
-    currentTile = map.getTileAt(1,1);
-
-    marker = this.add.graphics();
-    marker.lineStyle(2, 0x000000, 1);
-    marker.strokeRect(0, 0, map.tileWidth, map.tileHeight);
-
-    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-
-    var cursors = this.input.keyboard.createCursorKeys();
-    var controlConfig = {
-        camera: this.cameras.main,
-        left: cursors.left,
-        right: cursors.right,
-        up: cursors.up,
-        down: cursors.down,
-        speed: 0.5,
-    };
-
-    controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
-
-    var tiles = this.add.image(40,70,'tiles').setOrigin(0);
-    var sidemenuContainer = this.add.image(0,0,'sidemenu bkgd').setOrigin(0);
-    menuContainer = this.add.container(700, 0, [sidemenuContainer, tiles]);
-}
-
-function update (tile, delta) {
-    controls.update(delta);
-
-    var worldPoint = this.input.activePointer.positionToCamera(this.cameras.main);
-
-    var pointerTileX = map.worldToTileX(worldPoint.x);
-    var pointerTileY = map.worldToTileY(worldPoint.y);
-
-    marker.x = map.tileToWorldX(pointerTileX);
-    marker.y = map.tileToWorldY(pointerTileY);
-
-    if (this.input.manager.activePointer.isDown) {
-        map.putTileAt(currentTile, pointerTileX, pointerTileY);
-    }
-}
+export default new Phaser.Game(config);
